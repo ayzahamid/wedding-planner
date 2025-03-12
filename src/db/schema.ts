056c258d8852654088761f9aba698daf7190e045
Ayza Hamid <ayzahamid222@gmail.com>
2025-03-13 00:22:20 +0500

    Add drizzle orm to manage migrations and queries

---
+++ src/db/schema.ts
@@ -0,0 +1,14 @@
+import { integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+
+
+export const postsTable = pgTable('posts_table', {
+  id: serial('id').primaryKey(),
+  content: text('content').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at')
+    .notNull()
+    .$onUpdate(() => new Date()),
+});
+
+export type InsertPost = typeof postsTable.$inferInsert;
+export type SelectPost = typeof postsTable.$inferSelect;
